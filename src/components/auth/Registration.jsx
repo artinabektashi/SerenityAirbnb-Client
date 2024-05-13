da38fa7665b0c53ba99d181e758d01b9cff739f1
artinabektashi <ab51034@ubt-uni.net>
2024-05-13 21:05:23 +0200

    fix: registration form style

--- src/components/auth/Registration.jsx
+++ src/components/auth/Registration.jsx
@@ -35,7 +35,7 @@ const Registration = () => {
   };
 
   return (
-    <section className="container col-6 mt-5 mb-5">
+    <section className="login-container">
       {errorMessage && <p className="alert alert-danger">{errorMessage}</p>}
       {successMessage && (
         <p className="alert alert-success">{successMessage}</p>
@@ -43,79 +43,66 @@ const Registration = () => {
 
       <h2>Register</h2>
       <form onSubmit={handleRegistration}>
-        <div className="mb-3 row">
-          <label htmlFor="firstName" className="col-sm-2 col-form-label">
-            first Name
-          </label>
-          <div className="col-sm-10">
-            <input
-              id="firstName"
-              name="firstName"
-              type="text"
-              className="form-control"
-              value={registration.firstName}
-              onChange={handleInputChange}
-            />
-          </div>
+        <div className="form-group">
+          <label htmlFor="firstName">First Name</label>
+          <input
+            id="firstName"
+            name="firstName"
+            type="text"
+            className="form-control"
+            value={registration.firstName}
+            onChange={handleInputChange}
+          />
         </div>
 
-        <div className="mb-3 row">
-          <label htmlFor="lastName" className="col-sm-2 col-form-label">
-            Last Name
-          </label>
-          <div className="col-sm-10">
-            <input
-              id="lastName"
-              name="lastName"
-              type="text"
-              className="form-control"
-              value={registration.lastName}
-              onChange={handleInputChange}
-            />
-          </div>
+        <div className="form-group">
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            id="lastName"
+            name="lastName"
+            type="text"
+            className="form-control"
+            value={registration.lastName}
+            onChange={handleInputChange}
+          />
         </div>
 
-        <div className="mb-3 row">
-          <label htmlFor="email" className="col-sm-2 col-form-label">
-            Email
-          </label>
-          <div className="col-sm-10">
-            <input
-              id="email"
-              name="email"
-              type="email"
-              className="form-control"
-              value={registration.email}
-              onChange={handleInputChange}
-            />
-          </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            className="form-control"
+            value={registration.email}
+            onChange={handleInputChange}
+          />
         </div>
 
-        <div className="mb-3 row">
-          <label htmlFor="password" className="col-sm-2 col-form-label">
-            Password
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              name="password"
-              value={registration.password}
-              onChange={handleInputChange}
-            />
-          </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            value={registration.password}
+            onChange={handleInputChange}
+          />
         </div>
-        <div className="mb-3">
+        <div className="form-group">
           <button
             type="submit"
-            className="btn btn-hotel"
+            className="btn btn-primary"
             style={{ marginRight: "10px" }}
           >
             Register
           </button>
           <span style={{ marginLeft: "10px" }}>
-            Already have an account? <Link to={"/login"}>Login</Link>
+            Already have an account?{" "}
+            <Link className="register-link" to={"/login"}>
+              Login
+            </Link>
           </span>
         </div>
       </form>
